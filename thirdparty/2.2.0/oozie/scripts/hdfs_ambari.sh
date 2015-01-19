c6796d121ae90aba8861c283d65e303b2a12acdd
Anil Varkhedi <avarkhedi@veristorm.com>
2015-01-19 09:28:55 +0000

    hdfs_ambari.sh for the last time

--- thirdparty/2.2.0/oozie/scripts/hdfs_ambari.sh
+++ thirdparty/2.2.0/oozie/scripts/hdfs_ambari.sh
@@ -3,5 +3,5 @@
 # Temporary ambari fix to deploy solr/hue and oozie
 #
 echo HOSTNAME=${1} >/opt/vse/deploy.properties
-echo NODENAME=${2} >>/opt/vse/deploy.properties
+echo NAMENODE=${2} >>/opt/vse/deploy.properties
 echo JAVA_HOME=${3} >>/opt/vse/deploy.properties
